2efabf48e4f1c900c12e73e8e6de714ee0fb7b71
JonnyBoy2000 <jonathon_lokken@yahoo.com>
2021-11-16 13:19:01 -0600

    Update resume.js

--- musicBot/src/commands/resume.js
+++ musicBot/src/commands/resume.js
@@ -4,17 +4,17 @@ module.exports = {
     name: "resume",
     exec: async (msg) => {
         const { music } = msg.guild;
-        if (!music.player || !music.player.playing) return msg.channel.send("❌ Currently not playing anything.");
+        if (!music.player || !music.player.playing) return msg.channel.send("❌ Currently not playing anything.").then(msg => {msg.delete({timeout: 10000})});
         if (!msg.member.voice.channel)
-            return msg.channel.send("❌ You must be on a voice channel.");
+            return msg.channel.send("❌ You must be on a voice channel.").then(msg => {msg.delete({timeout: 10000})});
         if (msg.guild.me.voice.channel && !msg.guild.me.voice.channel.equals(msg.member.voice.channel))
-            return msg.channel.send(`❌ You must be on ${msg.guild.me.voice.channel} to use this command.`);
+            return msg.channel.send(`❌ You must be on ${msg.guild.me.voice.channel} to use this command.`).then(msg => {msg.delete({timeout: 10000})});
 
         try {
             await music.resume();
             msg.react("▶️").catch(e => e);
         } catch (e) {
-            msg.channel.send(`An error occured: ${e.message}.`);
+            msg.channel.send(`An error occured: ${e.message}.`).then(msg => {msg.delete({timeout: 10000})});
         }
     }
 };
