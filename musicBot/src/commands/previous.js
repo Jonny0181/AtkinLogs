97a72feb1738e399ef45a9310a8bab4592b578d9
JonnyBoy2000 <jonathon_lokken@yahoo.com>
2021-11-16 13:17:48 -0600

    Update previous.js

--- musicBot/src/commands/previous.js
+++ musicBot/src/commands/previous.js
@@ -5,13 +5,13 @@ module.exports = {
     aliases: ["prev"],
     exec: async (msg) => {
         const { music } = msg.guild;
-        if (!music.player) return msg.channel.send("❌ Currently not playing anything.");
-        if (!music.previous) return msg.channel.send("❌ No previous track.");
+        if (!music.player) return msg.channel.send("❌ Currently not playing anything.").then(msg => {msg.delete({timeout: 10000})});
+        if (!music.previous) return msg.channel.send("❌ No previous track.").then(msg => {msg.delete({timeout: 10000})});
 
         if (!msg.member.voice.channel)
-            return msg.channel.send("❌ You must be on a voice channel.");
+            return msg.channel.send("❌ You must be on a voice channel.").then(msg => {msg.delete({timeout: 10000})});
         if (msg.guild.me.voice.channel && !msg.guild.me.voice.channel.equals(msg.member.voice.channel))
-            return msg.channel.send(`❌ You must be on ${msg.guild.me.voice.channel} to use this command.`);
+            return msg.channel.send(`❌ You must be on ${msg.guild.me.voice.channel} to use this command.`).then(msg => {msg.delete({timeout: 10000})});
 
         try {
             music.queue.unshift(music.previous);
