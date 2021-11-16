b1383098363e0091e7cce4c96614cdec6b3d86e6
JonnyBoy2000 <jonathon_lokken@yahoo.com>
2021-11-16 13:20:10 -0600

    Update shuffle.js

--- musicBot/src/commands/shuffle.js
+++ musicBot/src/commands/shuffle.js
@@ -5,15 +5,15 @@ module.exports = {
     aliases: ["sf"],
     exec: async (msg) => {
         const { music } = msg.guild;
-        if (!music.player || !music.player.playing) return msg.channel.send("❌ Currently not playing anything.");
-        if (!music.queue.length) return msg.channel.send("❌ Queue is empty.");
+        if (!music.player || !music.player.playing) return msg.channel.send("❌ Currently not playing anything.").then(msg => {msg.delete({timeout: 10000})});
+        if (!music.queue.length) return msg.channel.send("❌ Queue is empty.").then(msg => {msg.delete({timeout: 10000})});
         if (!msg.member.voice.channel)
-            return msg.channel.send("❌ You must be on a voice channel.");
+            return msg.channel.send("❌ You must be on a voice channel.").then(msg => {msg.delete({timeout: 10000})});
         if (msg.guild.me.voice.channel && !msg.guild.me.voice.channel.equals(msg.member.voice.channel))
-            return msg.channel.send(`❌ You must be on ${msg.guild.me.voice.channel} to use this command.`);
+            return msg.channel.send(`❌ You must be on ${msg.guild.me.voice.channel} to use this command.`).then(msg => {msg.delete({timeout: 10000})});
 
         music.queue = util.shuffleArray(music.queue);
 
-        msg.channel.send(`✅ Queue shuffled! Type \`${msg.client.prefix}queue\` to see changes.`);
+        msg.channel.send(`✅ Queue shuffled! Type \`${msg.client.prefix}queue\` to see changes.`).then(msg => {msg.delete({timeout: 10000})});
     }
 };
