25ddd4414ff2e8b2cde84534bccba8a549ac7e2a
JonnyBoy2000 <jonathon_lokken@yahoo.com>
2021-11-16 13:18:44 -0600

    Update removedupes.js

--- musicBot/src/commands/removedupes.js
+++ musicBot/src/commands/removedupes.js
@@ -8,19 +8,19 @@ module.exports = {
         const { music } = msg.guild;
         const seen = {};
 
-        if (!music.player) return msg.channel.send("❌ Currently not playing anything.");
-        if (!music.queue.length) return msg.channel.send("❌ Queue is empty.");
+        if (!music.player) return msg.channel.send("❌ Currently not playing anything.").then(msg => {msg.delete({timeout: 10000})});
+        if (!music.queue.length) return msg.channel.send("❌ Queue is empty.").then(msg => {msg.delete({timeout: 10000})});
 
         if (!msg.member.voice.channel)
-            return msg.channel.send("❌ You must be on a voice channel.");
+            return msg.channel.send("❌ You must be on a voice channel.").then(msg => {msg.delete({timeout: 10000})});
         if (msg.guild.me.voice.channel && !msg.guild.me.voice.channel.equals(msg.member.voice.channel))
-            return msg.channel.send(`❌ You must be on ${msg.guild.me.voice.channel} to use this command.`);
+            return msg.channel.send(`❌ You must be on ${msg.guild.me.voice.channel} to use this command.`).then(msg => {msg.delete({timeout: 10000})});
             
         for (const song of music.queue) {
             if (seen[song.info.indentifier] === undefined) seen[song.info.indentifier] = song;
         }
         music.queue = Object.values(seen);
     
-        msg.channel.send("✅ Removed all Dupes").catch(e => e);
+        msg.channel.send("✅ Removed all Dupes").then(msg => {msg.delete({timeout: 10000})}).catch(e => e);
     }
 };
