19bde71e10092e91115c0d91c85f745720493748
JonnyBoy2000 <jonathon_lokken@yahoo.com>
2021-11-16 13:20:32 -0600

    Update skip.js

--- musicBot/src/commands/skip.js
+++ musicBot/src/commands/skip.js
@@ -6,21 +6,21 @@ module.exports = {
     exec: async (msg, args) => {
         const { music } = msg.guild;
         const skipTo = args[0] ? parseInt(args[0], 10) : null;
-        if (!music.player || !music.player.playing) return msg.channel.send("❌ Currently not playing anything.");
+        if (!music.player || !music.player.playing) return msg.channel.send("❌ Currently not playing anything.").then(msg => {msg.delete({timeout: 10000})});
 
         if (!msg.member.voice.channel)
-            return msg.channel.send("❌ You must be on a voice channel.");
+            return msg.channel.send("❌ You must be on a voice channel.").then(msg => {msg.delete({timeout: 10000})});
         if (msg.guild.me.voice.channel && !msg.guild.me.voice.channel.equals(msg.member.voice.channel))
-            return msg.channel.send(`❌ You must be on ${msg.guild.me.voice.channel} to use this command.`);
+            return msg.channel.send(`❌ You must be on ${msg.guild.me.voice.channel} to use this command.`).then(msg => {msg.delete({timeout: 10000})});
 
         if (skipTo !== null && (isNaN(skipTo) || skipTo < 1 || skipTo > music.queue.length))
-            return msg.channel.send("❌ Invalid number to skip.");
+            return msg.channel.send("❌ Invalid number to skip.").then(msg => {msg.delete({timeout: 10000})});
 
         try {
             await music.skip(skipTo);
             msg.react("⏭️").catch(e => e);
         } catch (e) {
-            msg.channel.send(`An error occured: ${e.message}.`);
+            msg.channel.send(`An error occured: ${e.message}.`).then(msg => {msg.delete({timeout: 10000})});
         }
     }
 };
