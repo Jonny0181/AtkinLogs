9ded5af77a55f97426c36b5906bf5a0c31e2a3e7
JonnyBoy2000 <jonathon_lokken@yahoo.com>
2021-11-16 13:18:27 -0600

    Update remove.js

--- musicBot/src/commands/remove.js
+++ musicBot/src/commands/remove.js
@@ -5,21 +5,21 @@ module.exports = {
     aliases: ["rm"],
     exec: async (msg, args) => {
         const { music } = msg.guild;
-        if (!music.player || !music.player.playing) return msg.channel.send("❌ Currently not playing anything.");
-        if (!music.queue.length) return msg.channel.send("❌ Queue is empty.");
+        if (!music.player || !music.player.playing) return msg.channel.send("❌ Currently not playing anything.").then(msg => {msg.delete({timeout: 10000})});
+        if (!music.queue.length) return msg.channel.send("❌ Queue is empty.").then(msg => {msg.delete({timeout: 10000})});
 
         if (!msg.member.voice.channel)
-            return msg.channel.send("❌ You must be on a voice channel.");
+            return msg.channel.send("❌ You must be on a voice channel.").then(msg => {msg.delete({timeout: 10000})});
         if (msg.guild.me.voice.channel && !msg.guild.me.voice.channel.equals(msg.member.voice.channel))
-            return msg.channel.send(`❌ You must be on ${msg.guild.me.voice.channel} to use this command.`);
+            return msg.channel.send(`❌ You must be on ${msg.guild.me.voice.channel} to use this command.`).then(msg => {msg.delete({timeout: 10000})});
 
-        if (!args[0]) return msg.channel.send("❌ Missing args.");
+        if (!args[0]) return msg.channel.send("❌ Missing args.").then(msg => {msg.delete({timeout: 10000})});
 
         let iToRemove = parseInt(args[0], 10);
         if (isNaN(iToRemove) || iToRemove < 1 || iToRemove > music.queue.length)
             return msg.channel.send("❌ Invalid number to remove.");
 
         const removed = music.queue.splice(--iToRemove, 1)[0];
-        msg.channel.send(`✅ Removed **${removed.info.title}** from the queue.`);
+        msg.channel.send(`✅ Removed **${removed.info.title}** from the queue.`).then(msg => {msg.delete({timeout: 10000})});
     }
 };
