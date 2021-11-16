f864e11a3e33cd46b53c04268d2b256149aa175e
JonnyBoy2000 <jonathon_lokken@yahoo.com>
2021-11-16 13:19:28 -0600

    Update search.js

--- musicBot/src/commands/search.js
+++ musicBot/src/commands/search.js
@@ -5,23 +5,23 @@ module.exports = {
     exec: async (msg, args) => {
         const { music } = msg.guild;
         if (!msg.member.voice.channel)
-            return msg.channel.send("❌ You must be on a voice channel.");
+            return msg.channel.send("❌ You must be on a voice channel.").then(msg => {msg.delete({timeout: 10000})});
         if (msg.guild.me.voice.channel && !msg.guild.me.voice.channel.equals(msg.member.voice.channel))
-            return msg.channel.send(`❌ You must be on ${msg.guild.me.voice.channel} to use this command.`);
+            return msg.channel.send(`❌ You must be on ${msg.guild.me.voice.channel} to use this command.`).then(msg => {msg.delete({timeout: 10000})});
 
         const missingPerms = util.missingPerms(msg.guild.me.permissionsIn(msg.member.voice.channel), ["CONNECT", "SPEAK"]);
         if ((!music.player || !music.player.playing) && missingPerms.length)
-            return msg.channel.send(`❌ I need ${missingPerms.length > 1 ? "these" : "this"} permission${missingPerms.length > 1 ? "s" : ""} on your voice channel: ${missingPerms.map(x => `\`${x}\``).join(", ")}.`);
+            return msg.channel.send(`❌ I need ${missingPerms.length > 1 ? "these" : "this"} permission${missingPerms.length > 1 ? "s" : ""} on your voice channel: ${missingPerms.map(x => `\`${x}\``).join(", ")}.`).then(msg => {msg.delete({timeout: 10000})});
 
         if (!music.node || !music.node.connected)
-            return msg.channel.send("❌ Lavalink node not connected.");
+            return msg.channel.send("❌ Lavalink node not connected.").then(msg => {msg.delete({timeout: 10000})});
 
         const query = args.join(" ");
-        if (!query) return msg.channel.send("❌ Missing args.");
+        if (!query) return msg.channel.send("❌ Missing args.").then(msg => {msg.delete({timeout: 10000})});
 
         try {
             let { tracks } = await music.load(`ytsearch:${query}`);
-            if (!tracks.length) return msg.channel.send("❌ Couldn't find any results.");
+            if (!tracks.length) return msg.channel.send("❌ Couldn't find any results.").then(msg => {msg.delete({timeout: 10000})});
 
             tracks = tracks.slice(0, 10);
 
@@ -54,7 +54,7 @@ module.exports = {
 
             music.setTextCh(msg.channel);
         } catch (e) {
-            msg.channel.send(`An error occured: ${e.message}.`);
+            msg.channel.send(`An error occured: ${e.message}.`).then(msg => {msg.delete({timeout: 10000})});
         }
 
         async function awaitMessages() {
