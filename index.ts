7e1161e93d6ecf16474df19ba068fb1b08a6ec7f
JonnyBoy2000 <jonathon_lokken@yahoo.com>
2021-12-08 03:26:18 -0600

    Add files via upload

--- index.ts
+++ index.ts
@@ -8,6 +8,7 @@ const client = new DiscordJS.Client({
     intents: [
         Intents.FLAGS.GUILDS,
         Intents.FLAGS.GUILD_MESSAGES,
+        Intents.FLAGS.GUILD_MEMBERS,
         Intents.FLAGS.GUILD_MESSAGE_REACTIONS
     ]
 })
@@ -16,6 +17,7 @@ client.on('ready', () => {
     console.log('The bot is ready')
     new WOKCommands(client, {
         commandsDir: path.join(__dirname, 'commands'),
+        featuresDir: path.join(__dirname, 'features'),
         showWarns: true,
         typeScript: true,
         testServers: ['864624899292266526', '678226695190347797'],
